0db64e0f44bcf819573e9baf4260e4157047857d
ali reza azdast <azdast47@gmail.com>
2025-04-15 10:40:05 +0330

    finished a grid section add skill and fixed but of a back ground gradient

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./BackgroundGradientAnimation";
 import { GlobeDemo } from "./GridGlobe";
-
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from '@/data/confetti.json'
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -41,11 +47,20 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 
+
+
 }) => {
+
+  const [copied, setCopied] = useState(false)
+  const handleCopy = () => {
+    navigator.clipboard.writeText('azdast47@gmail.com');
+
+    setCopied(true);
+  }
   return (
     <div
       className={cn(
-        "group/bento relative overflow-hidden shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento relative overflow-hidden shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border border-white-[0.1] transition duration-200 hover:shadow-xl",
         className,
       )}
       style={{
@@ -79,9 +94,8 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
           // add background animation , remove the p tag
-          <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
-          </BackgroundGradientAnimation>
+          <BackgroundGradientAnimation/>
+          
         )}
         <div className={cn(
           titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
@@ -92,14 +106,51 @@ export const BentoGridItem = ({
           <div className=" font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
+
+          {id == 2 && (<GlobeDemo />)}
+          {id == 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['react.js', 'next.js', 'typescript'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]">
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+                {['django', 'rest API', 'gsap'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]">
+                    {item}
+                  </span>
+                ))}
+
+              </div>
+            </div>
+          )}
+          {id == 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0 `}>
+                <Lottie options={{
+                  loop: copied,
+                  autoplay: copied,
+                  animationData: animationData,
+                  rendererSettings: {
+                    preserveAspectRatio: 'xMidyMid slice',
+                  }
+                }}/>
+              </div>
+              <MagicButton 
+                title={copied ? 'email copyed': 'copy my email'}
+                icon={<IoCopyOutline/>}
+                position="left"
+                otherClasses="!bg-[161a31] "
+                handleClick={handleCopy}
+              />
+            </div>
+          )}
         </div>
-        {id == 2 && (<GlobeDemo/>)}
-      </div>
-
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-
-
-
       </div>
     </div>
   );
